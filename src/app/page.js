d36188a40e786fb7f33faa3f093834b2249ca278
Reactongraph <js.developer@ongraph.com>
2023-12-26 16:32:20 +0530

    setup with nextjs + tailwind css + signup page

---
+++ src/app/page.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <>
+      <main className="p-[120px]">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center justify-center gap-[12px]">
+            <p className="font-mont text-[48px] font-semibold leading-[56px]">
+              My movies
+            </p>
+            <img src="/addcircle.svg" alt="image" />
+          </div>
+          <div></div>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ src/components/Footer/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <img src="/footer.svg" className="w-full" />;
+};
+
+export default Footer;
